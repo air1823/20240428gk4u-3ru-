aeb16dcaa024c41721e2abb5e0669df25958c9be
air1823 <413730549@o365.tku.edu.tw>
2025-04-28 09:45:31 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+let video;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  video = createCapture(VIDEO); // 擷取攝影機影像
+  video.size(windowWidth * 0.85, windowHeight * 0.85); // 設定影像大小為視窗的 85%
+  video.hide(); // 隱藏原始的 HTML 元素
+}
+
+function draw() {
+  background("#ffffb7"); // 設定背景顏色為淺黃色
+  // 計算影像的顯示位置，使其置中
+  let x = (width - video.width) / 2;
+  let y = (height - video.height) / 2;
+  image(video, x, y); // 在畫布上繪製攝影機影像
+}
